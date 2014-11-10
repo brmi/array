e18f0c9e3d86ae12a15d37e06ffc8d925d7b75af
desiree <localadmin@s-169-232-107-26.resnet.ucla.edu>
2014-11-09 21:20:38 -0800

    Initial Commit

---
+++ array/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  array
+//
+//  Created by desiree on 11/9/14.
+//  Copyright (c) 2014 desiree. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
